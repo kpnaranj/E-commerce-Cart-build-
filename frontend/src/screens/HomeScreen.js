20bd8f6f0f0ff1d32942a63ecda3ebf902ab28bd
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-01-16 06:41:39 -0500

    Completing cart product - beginning

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import "./HomeScreen.css"
+import React from "react";
+import "./HomeScreen.css";
+import Product from "../components/Product";
 
 function HomeScreen() {
-    return (
-        <div className="homescreen">
-            HomeScreen
-        </div>
-    )
+  return (
+    <div className="homescreen">
+      <h2 className="homescreen__title">Latest Products</h2>
+      <div className="homescreen__products">
+        <Product />
+      </div>
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
