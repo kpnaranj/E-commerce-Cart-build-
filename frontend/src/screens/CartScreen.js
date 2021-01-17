968d510cb61b612c1df65debed339d22c9bdbf23
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-01-17 05:46:21 -0500

    Development of cart items

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import CartItem from "../components/CartItem";
 
 function CartScreen() {
-    return (
-        <div className="cartscreen">
-            CartScreen
+  return (
+    <div className="cartscreen">
+      <div className="cartscreen__left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+      </div>
+      <div className="cartscreen__right">
+        <div className="cartscreen__info">
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
         </div>
-    )
+      </div>
+      <div>
+        <button>Proceed to Checkout</button>
+      </div>
+    </div>
+  );
 }
 
-export default CartScreen
+export default CartScreen;
