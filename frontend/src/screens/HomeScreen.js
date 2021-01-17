d02e081630a146f48c3127fba5aa5163023ce496
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-01-17 04:50:20 -0500

    Mobile responsive css configuration of products in homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,6 +8,11 @@ function HomeScreen() {
       <h2 className="homescreen__title">Latest Products</h2>
       <div className="homescreen__products">
         <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
       </div>
     </div>
   );
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ProductScreen() {
     return (
         <div className="productscreen">
-            ProductScreen
+           
         </div>
     )
 }
